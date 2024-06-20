9039ff2c72ee010d30956edf6ec4d6895343fa2d
Mohammed Abed <63855373+mabedd@users.noreply.github.com>
2024-06-20 03:13:40 +0300

    Start TraceBeam

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Clients from "@/components/Clients";
 import MarkettingLamp from "@/components/MarkettingLamp";
 import Footer from "@/components/Footer";
 import Process from "@/components/Process";
+import { TracingBeam } from "@/components/accternity/TracingBeam";
 import { FloatingNav } from "@/components/accternity/FloatingNav";
 
 import { navItems } from "@/data";
@@ -17,16 +18,18 @@ export default function Home() {
     <>
       <Hero />
       <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-        <div className="max-w-7xl w-full">
-          <FloatingNav navItems={navItems} />
-          <Grid />
-          <MarkettingLamp />
-          <RecentProjects />
-          {/* <Clients /> */}
-          <Experience />
-          <Process />
-          <Footer />
-        </div>
+        <TracingBeam>
+          <div className="max-w-7xl w-full">
+            <FloatingNav navItems={navItems} />
+            <Grid />
+            <MarkettingLamp />
+            <RecentProjects />
+            {/* <Clients /> */}
+            <Experience />
+            <Process />
+            <Footer />
+          </div>
+        </TracingBeam>
       </main>
     </>
   );
